bca624d8dc88b336b38f28cba0cba1c23023a0ab
Barroso18 <david.barrosomartinez@gmail.com>
2025-02-18 09:51:55 +0100

    Continuamos con los tutoriales

--- RESTfulWebService/src/main/java/goya/daw2/ud8/Quote.java
+++ RESTfulWebService/src/main/java/goya/daw2/ud8/Quote.java
@@ -2,6 +2,26 @@ package goya.daw2.ud8;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record Quote(String type, Value value) {
+public class Quote {
+	String type;
 
+	@Override
+	public String toString() {
+		return "Quote [type=" + type + "]";
+	}
+	Value value;
+
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public Value getValue() {
+		return value;
+	}
+	public void setValue(Value value) {
+		this.value = value;
+	}
+	
 }
--- RESTfulWebService/src/main/java/goya/daw2/ud8/Value.java
+++ RESTfulWebService/src/main/java/goya/daw2/ud8/Value.java
@@ -2,6 +2,24 @@ package goya.daw2.ud8;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record Value(Long id, String quote) {
-
+public class Value{
+	Long id;
+	String quote;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getQuote() {
+		return quote;
+	}
+	public void setQuote(String quote) {
+		this.quote = quote;
+	}
+	@Override
+	public String toString() {
+		return "Value [id=" + id + ", quote=" + quote + "]";
+	}
+	
 }
